e14eda73fcfc0ed836e6a7016c3f78411411eafb
mdato <ciel2000@gmail.com>
2022-09-15 21:33:30 -0300

    style2

--- src/components/auth/User8.tsx
+++ src/components/auth/User8.tsx
@@ -6,9 +6,9 @@ export type UserProps = {
 export const User8 = ({ alias, country }: UserProps) => {
   return (
     <div>
-      🔐​ My name is 
-      <span style={{ fontWeight: "bolder", fontSize: "18px" }}>{alias}</span>
-       and I am from ​🇩🇰​🌍 
+      🔐​ My name is
+      <span style={{ fontWeight: "bolder", fontSize: "18px" }}> {alias} </span>
+      and I am from ​🇩🇰​🌍
       <span style={{ color: "#f8f8f8", backgroundColor: "red" }}>
         {country}
       </span>
